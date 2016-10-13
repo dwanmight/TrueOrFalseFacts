2e8a1a5c3960cfbca6b92b958d72e55eb8c7c0b7
dwanmight <dwanmight@gmail.com>
2016-10-13 11:15:37 +0300

    Add manager for constant

---
+++ geoquiz/src/main/java/com/junior/dwan/geoquiz/utils/ConstantManager.java
@@ -0,0 +1,35 @@
+package com.junior.dwan.geoquiz.utils;
+
+/**
+ * Created by Might on 13.10.2016.
+ */
+
+public interface ConstantManager {
+    String JSON_ISTRUE = "istrue";
+    String JSON_QUESTION = "question";
+    
+    String EXTRA_START_GAME = "com.junior.dwan.geoquiz.start_game";
+    String SAVED_HScore = "hscore";
+
+    String KEY_INDEX = "index";
+    String KEY_SCORE = "score";
+    String KEY_ANSWER = "answer";
+    String KEY_CHEAT = "cheat";
+    String KEY_POINT = "point";
+
+    String TAG = "tag";
+
+    String SHARED_PREFERENCES = "sPref";
+    String SAVED_INDEX = "saved_index";
+    String SAVED_SCORE = "saved_score";
+    String SAVED_POINT = "saved_point";
+    String SAVED_PASS = "saved_pass";
+    String SAVED_CHEATER = "saved_cheater";
+    String SAVED_ANSWER = "saved_answer";
+
+    int DIALOG_RESET = 1;
+
+    String EXTRA_SCORE = "score";
+    String EXTRA_HIGHSCORE = "highscore";
+}
+
